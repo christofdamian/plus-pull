e5b98589273c70c59376a4186e3dc9d9cc4999e5
Christof Damian <christof@damian.net>
2014-04-29 17:03:21 +0200

    indentation fixes

--- src/PlusPull/Commands/Check.php
+++ src/PlusPull/Commands/Check.php
@@ -58,71 +58,73 @@ class Check extends AbstractCommand
         $maxPulls = $input->getOption('limit');
 
         if (!empty($config['repository'])) {
-          $repositories = array($config['repository']);
+            $repositories = array($config['repository']);
         } else {
-          $repositories = $config['repositories'];
+            $repositories = $config['repositories'];
         }
 
         foreach ($repositories as $repositoryConfig) {
 
-          $username = $repositoryConfig['username'];
-          $repository = $repositoryConfig['name'];
-          $checkStatus = !empty($repositoryConfig['status']);
+            $username = $repositoryConfig['username'];
+            $repository = $repositoryConfig['name'];
+            $checkStatus = !empty($repositoryConfig['status']);
 
-          $output->writeln("repository: $username/$repository");
+            $output->writeln("repository: $username/$repository");
 
-          $plusRequired = 3;
-          if (!empty($repositoryConfig['required'])) {
-              $plusRequired = $repositoryConfig['required'];
-          }
+            $plusRequired = 3;
+            if (!empty($repositoryConfig['required'])) {
+                $plusRequired = $repositoryConfig['required'];
+            }
 
-          $whitelist = null;
-          if (!empty($repositoryConfig['whitelist'])) {
-              $whitelist = $repositoryConfig['whitelist'];
-          }
+            $whitelist = null;
+            if (!empty($repositoryConfig['whitelist'])) {
+                $whitelist = $repositoryConfig['whitelist'];
+            }
 
-          $github->setRepository($username, $repository);
+            $github->setRepository($username, $repository);
 
-          foreach ($github->getPullRequests() as $pullRequest) {
-              $pull = $input->getOption('pull');
+            foreach ($github->getPullRequests() as $pullRequest) {
+                $pull = $input->getOption('pull');
 
-              $output->write($pullRequest->number.' ('.$pullRequest->title.')');
+                $output->write(
+                    $pullRequest->number.' ('.$pullRequest->title.')'
+                );
 
-              if ($pullRequest->checkComments($plusRequired, $whitelist)) {
-                  $output->write(' +1');
-              } else {
-                  $output->write(' -1');
-                  $pull = false;
-              }
+                if ($pullRequest->checkComments($plusRequired, $whitelist)) {
+                    $output->write(' +1');
+                } else {
+                    $output->write(' -1');
+                    $pull = false;
+                }
 
-              if ($checkStatus) {
-                  if ($pullRequest->checkStatuses()) {
-                      $output->write(' success');
-                  } else {
-                      $output->write(' fail');
-                      $pull = false;
-                  }
-              }
+                if ($checkStatus) {
+                    if ($pullRequest->checkStatuses()) {
+                        $output->write(' success');
+                    } else {
+                        $output->write(' fail');
+                        $pull = false;
+                    }
+                }
 
-              if ($pullRequest->isMergeable()) {
-                  $output->write(' mergeable');
-              } else {
-                  $output->write(' conflicts');
-                  $pull = false;
-              }
+                if ($pullRequest->isMergeable()) {
+                    $output->write(' mergeable');
+                } else {
+                    $output->write(' conflicts');
+                    $pull = false;
+                }
 
-              if ($pull) {
-                  $github->merge($pullRequest->number);
-                  $output->write(' pulled');
-                  $maxPulls--;
-              }
+                if ($pull) {
+                    $github->merge($pullRequest->number);
+                    $output->write(' pulled');
+                    $maxPulls--;
+                }
 
-              $output->writeln('');
+                $output->writeln('');
 
-              if ($maxPulls<=0) {
-                  break;
-              }
-          }
-       }
+                if ($maxPulls<=0) {
+                    break;
+                }
+            }
+        }
     }
 }
